885254bfef23e4932066d8c9cadb8aa77f7a09dd
Aline Schroeder <aliineschroeder@gmail.com>
2024-05-01 11:18:33 -0300

    Formulário

--- main.js
+++ main.js
@@ -58,6 +58,18 @@ function addKeyboardEventListeners(){
     })
 }
 
+
+function selectCard(selector) {
+    var element = document.querySelector(selector);
+    element.classList.toggle("card-selected");
+    if(ingressos.includes(selector)) ingressos.pop(selector);
+    else ingressos.push(selector)
+}
+
+showSelectedCards = () => {
+    if(ingressos.length > 0) alert("Ingressos Selecionados:" + ingressos);
+}
+
 addKeyboardEventListeners();
 
 //checkKeyboardCode();
